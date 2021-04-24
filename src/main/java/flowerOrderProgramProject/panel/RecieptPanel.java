fda79fc8ef852a98781449bf7a939caf915a9f4c
이민정 <@DESKTOP-CMQ012O>
2021-04-24 22:31:44 +0900

    20210424 밤10시 30분, 영수증 만드는중

--- src/main/java/flowerOrderProgramProject/panel/RecieptPanel.java
+++ src/main/java/flowerOrderProgramProject/panel/RecieptPanel.java
@@ -6,30 +6,68 @@ import javax.swing.JPanel;
 import flowerOrderProgramProject.dto.Order_program;
 
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class RecieptPanel extends JPanel {
 	private JLabel lbl1;
 	private JLabel lbl2;
 	private JLabel lbl3;
 	private JLabel lbl4;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_3;
+	private JLabel lbl5;
 
 	public RecieptPanel() {
 
 		initialize();
 	}
 	private void initialize() {
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new GridLayout(0, 2, 0, 0));
 		
-		lbl1 = new JLabel("New label");
+		lblNewLabel_4 = new JLabel("▒ Order_Date >>>  ");
+		lblNewLabel_4.setFont(new Font("Century Gothic", Font.BOLD, 15));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.LEFT);
+		add(lblNewLabel_4);
+		
+		lbl1 = new JLabel("");
 		add(lbl1);
 		
-		lbl2 = new JLabel("New label");
+		lblNewLabel = new JLabel("▒ Flower_code >>>  ");
+		lblNewLabel.setFont(new Font("Century Gothic", Font.BOLD, 15));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel.setIcon(null);
+		add(lblNewLabel);
+		
+		lbl2 = new JLabel("");
 		add(lbl2);
 		
-		lbl3 = new JLabel("New label");
+		lblNewLabel_1 = new JLabel("▒ Order_count >>>  ");
+		lblNewLabel_1.setFont(new Font("Century Gothic", Font.BOLD, 15));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		add(lblNewLabel_1);
+		
+		lbl3 = new JLabel("");
 		add(lbl3);
 		
-		lbl4 = new JLabel("New label");
+		lblNewLabel_3 = new JLabel("▒ Choice >>>");
+		lblNewLabel_3.setFont(new Font("Century Gothic", Font.BOLD, 15));
+		add(lblNewLabel_3);
+		
+		lbl5 = new JLabel("");
+		add(lbl5);
+		
+		lblNewLabel_2 = new JLabel("▒ Sale_Price >>>  ");
+		lblNewLabel_2.setFont(new Font("Century Gothic", Font.BOLD, 15));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
+		add(lblNewLabel_2);
+		
+		lbl4 = new JLabel("");
 		add(lbl4);
 	}
 	
--- src/main/java/flowerOrderProgramProject/ui/content/Reciept.java
+++ src/main/java/flowerOrderProgramProject/ui/content/Reciept.java
@@ -10,11 +10,19 @@ import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import flowerOrderProgramProject.panel.RecieptPanel;
+import javax.swing.ImageIcon;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Reciept extends JFrame {
 
 	private JPanel contentPane;
 	private RecieptPanel panel_1;
+	private JButton button;
+	private JLabel lblNewLabel;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -33,24 +41,42 @@ public class Reciept extends JFrame {
 		initialize();
 	}
 	private void initialize() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 294, 377);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 711, 398);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 266, 221);
-		contentPane.add(panel);
-		panel.setLayout(new GridLayout(1, 0, 0, 0));
-		
 		panel_1 = new RecieptPanel();
-		panel.add(panel_1);
+		panel_1.setBounds(12, 10, 405, 343);
+		contentPane.add(panel_1);
+		
+		button = new JButton("");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionPerformedButton(e);
+			}
+		});
+		button.setBackground(Color.WHITE);
+		button.setIcon(new ImageIcon("D:\\flowerOrderProgramProject\\flowerOrderProgramProject\\image\\1111111111.jpg"));
+		button.setBounds(429, 286, 67, 67);
+		contentPane.add(button);
+		
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setIcon(new ImageIcon("D:\\flowerOrderProgramProject\\flowerOrderProgramProject\\image\\flower\\IMG_00061.jpg"));
+		lblNewLabel.setBounds(0, -15, 695, 374);
+		contentPane.add(lblNewLabel);
 	}
 
 	public RecieptPanel getPanel_1() {
 		return panel_1;
 	}
+	protected void actionPerformedButton(ActionEvent e) {
+		dispose();
+	}
+	
 	
 }
