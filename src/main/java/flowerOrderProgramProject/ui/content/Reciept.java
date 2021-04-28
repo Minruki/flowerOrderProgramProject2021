9a777bca139dbdea21df3bac3de6a31ce29f90bf
이민정 <@DESKTOP-CMQ012O>
2021-04-29 08:29:32 +0900

    20210429

--- src/main/java/flowerOrderProgramProject/ui/content/Reciept.java
+++ src/main/java/flowerOrderProgramProject/ui/content/Reciept.java
@@ -1,29 +1,33 @@
 package flowerOrderProgramProject.ui.content;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import flowerOrderProgramProject.panel.RecieptPanel;
-import javax.swing.ImageIcon;
-import javax.swing.BoxLayout;
-import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
+import flowerOrderProgramProject.panel.RecieptPanel;
+
+@SuppressWarnings("serial")
 public class Reciept extends JFrame {
 
 	private JPanel contentPane;
 	private RecieptPanel panel_1;
 	private JButton button;
-	private JButton btnNewButton;
 	private JLabel lblNewLabel;
+	private JButton printComponent;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -67,11 +71,16 @@ public class Reciept extends JFrame {
 		button.setBounds(504, 286, 67, 67);
 		contentPane.add(button);
 		
-		btnNewButton = new JButton("");
-		btnNewButton.setBackground(Color.WHITE);
-		btnNewButton.setIcon(new ImageIcon("C:\\workspace_proj\\flowerOrderProgramProject (8)\\flowerOrderProgramProject\\flowerOrderProgramProject2021\\image\\프린터.jpg"));
-		btnNewButton.setBounds(423, 286, 78, 67);
-		contentPane.add(btnNewButton);
+		printComponent = new JButton("");
+		printComponent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				actionPerformedBtnNewButton(arg0);
+			}
+		});
+		printComponent.setBackground(Color.WHITE);
+		printComponent.setIcon(new ImageIcon("C:\\workspace_proj\\flowerOrderProgramProject (8)\\flowerOrderProgramProject\\flowerOrderProgramProject2021\\image\\프린터.jpg"));
+		printComponent.setBounds(423, 286, 78, 67);
+		contentPane.add(printComponent);
 		
 		lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\workspace_proj\\flowerOrderProgramProject (8)\\flowerOrderProgramProject\\flowerOrderProgramProject2021\\image\\1231.jpg"));
@@ -87,4 +96,34 @@ public class Reciept extends JFrame {
 	}
 	
 	
-}
+	protected void actionPerformedBtnNewButton(ActionEvent arg0) {
+		printComponent();
+	}
+	
+	
+	
+	
+	public void printComponent() {
+		PrinterJob pj = PrinterJob.getPrinterJob();
+		pj.setJobName("print Component");
+		
+		pj.setPrintable(new Printable() {
+			public int print(Graphics pg, PageFormat pf, int pageNum) {
+				if(pageNum > 0) return Printable.NO_SUCH_PAGE;
+				
+				Graphics2D g2 = (Graphics2D) pg;
+				return  Printable.PAGE_EXISTS;
+			}
+				
+			});
+		
+			if(pj.printDialog() == false) return;
+			
+			try {
+				pj.print();
+				
+			}catch (PrinterException ex) {
+			}
+	}
+	}
+
